e40b6e4c5d28b987dc29dbbf2b9239db84af16f0
Jon Oram <jonathan.oram@navy.mil>
2022-12-12 13:25:30 -0500

    Changed name

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Gary's Tanzu!";
+		return "Greetings from Spring Boot + Jon's Tanzu!";
 	}
 
 }
